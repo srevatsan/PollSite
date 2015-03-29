3f7e85ea56260bcd4907b588d1732c496744cdec
Sri Muralidharan <srevatsan@edmodo.com>
2015-03-29 13:57:53 -0700

    Adding changes to admin page with tab based user experience to add questions and choice

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PollSite
 Demo Poll application for Django
+
+
+Install mysql 
+
--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'topic', 'pub_date')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    search_fields = ['question_text', 'topic']
+
+admin.site.register(Question, QuestionAdmin)
+
+
