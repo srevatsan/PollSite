0eaa0f0eb7dc26d1d09e27d2c318cfa9041d3fa2
Srevatsan Muralidharan <srevatsan_muralidharan@intuit.com>
2015-04-04 21:44:40 -0700

    tests

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,23 @@
+import datetime
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     topic = models.CharField(max_length=200, null=True)
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
     def __str__(self):
         return self.question_text
+
+
 
 
 class Choice(models.Model):
